08361b1b9397fb07a4cce440bc8cc5b0d999118b
deekshith <deekshith207@gmail.com>
2016-04-19 10:24:49 -0400

    Second Commit

---
+++ Hello/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: Hello
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+file '/tmp/helloworld.txt' do
+  content 'hello world'
+end
